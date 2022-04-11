d74759c213175d6115a9d2faed3936cfdf5476fa
Krishnavyas Muddineni <krishna.muddineni@shopee.com>
2022-04-11 10:13:13 +0800

    new trained dqn model.

--- main.py
+++ main.py
@@ -2,42 +2,63 @@ from pettingzoo.atari import boxing_v1
 from utils import *
 from tqdm import tqdm
 from model import DeepQNetwork
+from stable_baselines3 import DQN
+import numpy as np
 
 ## defining the environment properties.
 env = boxing_v1.parallel_env()
 
 env = preset_settings_env(env)
 
+model = DQN.load('dqn.model')
 
 def main():
     observations = env.reset()
     max_cycles = 50000
-    DQN = DeepQNetwork(env, env.agents[0])
+    #DQN = DeepQNetwork(env, env.agents[0])
+
+    # for step in tqdm(range(max_cycles), total=max_cycles):
+    #     prev_obs = observations
+    #     actions = {DQN.agent_name: DQN.choose_action(observations[DQN.agent_name].transpose(2,0,1)), env.agents[1]: policy(observations, env.agents[1])}
+    #     #actions = {agent: policy(observations[agent], agent) for agent in env.agents}
+    #     observations, rewards, dones, infos = env.step(actions)
+    #     ## process related to dqn agent
+
+    #     DQN.store_transition(observations[DQN.agent_name].transpose(2,0,1), actions[DQN.agent_name], rewards[DQN.agent_name], prev_obs[DQN.agent_name].transpose(2,0,1), dones[DQN.agent_name])
+    #     if len(DQN.memory)>1000 and DQN.state_counter%4 :
+    #         loss,avg_q = DQN.learn()
+
+    #     if DQN.state_counter%10000 ==0:
+    #         DQN.target_net.load_state_dict(DQN.eval_net.state_dict())
+
+    #     if len(env.agents) < 2:
+    #         ## reset environment and play again.
+    #         observations = env.reset()
+
+    #     env.render()
 
     for step in tqdm(range(max_cycles), total=max_cycles):
-        prev_obs = observations
-        actions = {DQN.agent_name: DQN.choose_action(observations[DQN.agent_name].transpose(2,0,1)), env.agents[1]: policy(observations, env.agents[1])}
+        actions = {env.agents[0]: dqn_policy(observations[env.agents[0]]),
+                   env.agents[1]: random_policy(observations[env.agents[1]], env.agents[1])}
+
         #actions = {agent: policy(observations[agent], agent) for agent in env.agents}
         observations, rewards, dones, infos = env.step(actions)
-        ## process related to dqn agent
-
-        DQN.store_transition(observations[DQN.agent_name].transpose(2,0,1), actions[DQN.agent_name], rewards[DQN.agent_name], prev_obs[DQN.agent_name].transpose(2,0,1), dones[DQN.agent_name])
-        if len(DQN.memory)>1000 and DQN.state_counter%4 :
-            loss,avg_q = DQN.learn()
-
-        if DQN.state_counter%10000 ==0:
-            DQN.target_net.load_state_dict(DQN.eval_net.state_dict())
 
         if len(env.agents) < 2:
             ## reset environment and play again.
+            break
             observations = env.reset()
 
         env.render()
 
 
-def policy(obsv, agent):
+
+def random_policy(obsv, agent):
     return env.action_space(agent).sample()
 
+def dqn_policy(obs):
+    action, _ = model.predict(obs,deterministic=True)
+    return action
 
 if __name__ == '__main__':
     main()
